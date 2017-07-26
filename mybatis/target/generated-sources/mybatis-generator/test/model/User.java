4a2cddec5d65c1748476b9f1f0187692747289ab
wuli2496 <wuli2496@163.com>
2017-07-26 22:31:24 +0800

    添加mybatis-generator工程

---
+++ mybatis/target/generated-sources/mybatis-generator/test/model/User.java
@@ -0,0 +1,95 @@
+package test.model;
+
+import java.util.Date;
+
+public class User {
+    private Integer uid;
+
+    private String email;
+
+    private String name;
+
+    private String password;
+
+    private Short status;
+
+    private Short activateFlag;
+
+    private String mobile;
+
+    private Date createTime;
+
+    private Date modifyTime;
+
+    public Integer getUid() {
+        return uid;
+    }
+
+    public void setUid(Integer uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password == null ? null : password.trim();
+    }
+
+    public Short getStatus() {
+        return status;
+    }
+
+    public void setStatus(Short status) {
+        this.status = status;
+    }
+
+    public Short getActivateFlag() {
+        return activateFlag;
+    }
+
+    public void setActivateFlag(Short activateFlag) {
+        this.activateFlag = activateFlag;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile == null ? null : mobile.trim();
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getModifyTime() {
+        return modifyTime;
+    }
+
+    public void setModifyTime(Date modifyTime) {
+        this.modifyTime = modifyTime;
+    }
+}
